b4304c9fa4e43241fc3fc3082b46787aa50b9845
Shannon TJ <shannontj@shaw.ca>
2020-12-21 20:56:45 -0700

    home page

--- src/components/pages/HomePage/Home.js
+++ src/components/pages/HomePage/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import HeroSection from "../../HeroSection";
-import { homeObjOne } from "./Data";
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "./Data";
 
 function Home() {
   return (
     <>
       <HeroSection {...homeObjOne} />
+      <HeroSection {...homeObjThree} />
+      <HeroSection {...homeObjTwo} />
+      <HeroSection {...homeObjFour} />
     </>
   );
 }
