2546e4e57ee96b97e080a837f73c315c6f7b9466
Shannon TJ <shannontj@shaw.ca>
2020-12-21 20:17:31 -0700

    herosection setup

---
+++ src/components/HeroSection.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./HeroSection.css";
+
+function HeroSection() {
+  return <></>;
+}
+
+export default HeroSection;
