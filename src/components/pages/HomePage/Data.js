561133037fd6027508041cbdc15c144cdb7b149e
Shannon TJ <shannontj@shaw.ca>
2020-12-21 21:01:24 -0700

    footer setup

--- src/components/pages/HomePage/Data.js
+++ src/components/pages/HomePage/Data.js
@@ -8,7 +8,7 @@ export const homeObjOne = {
     "Get access to our exclusive diamond card that allows you to  send unlimited transactions without getting charged any fees",
   buttonLabel: "Get Started",
   imgStart: "",
-  img: "images/svg-1.svg",
+  src: "images/svg-1.svg",
   alt: "Credit Card",
 };
 
@@ -22,7 +22,7 @@ export const homeObjTwo = {
     "We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime",
   buttonLabel: "Learn More",
   imgStart: "",
-  img: "images/svg-5.svg",
+  src: "images/svg-5.svg",
   alt: "Vault",
 };
 
@@ -36,7 +36,7 @@ export const homeObjThree = {
     "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
   buttonLabel: "Start Now",
   imgStart: "start",
-  img: "images/svg-7.svg",
+  src: "images/svg-7.svg",
   alt: "Vault",
 };
 
@@ -50,6 +50,6 @@ export const homeObjFour = {
     "Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.",
   buttonLabel: "Sign Up Now",
   imgStart: "start",
-  img: "images/svg-8.svg",
+  src: "images/svg-8.svg",
   alt: "Vault",
 };
